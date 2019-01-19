7363a9ecee51b42f408755fcfaa95fdda7be62c9
sanchows <sanyasach@mail.ru>
2019-01-19 18:15:30 +0300

    Fixed bugs in server

--- Server.py
+++ Server.py
@@ -12,11 +12,11 @@ class ClientThread(threading.Thread):
         while True:
             if not isConn: 
                 print ('Подключился клиент: ', addr, end = ' Название: ')
-                data = conn.recv(1024)
+                data = self.conn.recv(1024)
                 isConn = True
                 print(data.decode("utf-8"))
             
-            replyFromClient = conn.recv(1024)
+            replyFromClient = self.conn.recv(1024)
             
             if not replyFromClient:
                 break
@@ -25,13 +25,13 @@ class ClientThread(threading.Thread):
             
             if replyFromClient.decode('utf-8')=='/exit':
                 break         
-            conn.send(replyFromClient.decode("utf-8").upper().encode())
+            self.conn.send(replyFromClient.decode("utf-8").upper().encode())
         conn.close()
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 9090))
-server.listen(5)
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn.bind(('', 9090))
+conn.listen(5)
 
-while True:
-    conn, addr = server.accept()
-    ClientThread(conn, addr).start()
+while True: 
+    connS, addr = conn.accept()
+    ClientThread(connS, addr).start()
