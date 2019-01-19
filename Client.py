22a4c8f2acd225b2515a08d67a8b58bd88faa30a
sanchows <sanyasach@mail.ru>
2019-01-19 15:46:19 +0300

    Fix bags.

--- Client.py
+++ Client.py
@@ -1,8 +1,7 @@
-import pickle
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 9091))
+sock.connect(('localhost', 9090))
 
 print ('You are connected to server. Enter your name: ', end='')
 clientName = input()
@@ -11,11 +10,10 @@ while True:
     print (clientName + ': ', end = '')
     textMsg = str(input())
     sock.send(textMsg.encode())
-    
     replyFromServer = sock.recv(1024)
-    print('Server: ' + str(replyFromServer.decode("utf-8")))
     if textMsg == '/exit':
         break
+    print('Server: ' + str(replyFromServer.decode("utf-8")))
 
 
 sock.close()
--- Server.py
+++ Server.py
@@ -1,4 +1,3 @@
-import pickle
 import socket
 import threading
 
@@ -11,23 +10,27 @@ class ClientThread(threading.Thread):
     def run(self):
         isConn = False
         while True:
-            if not isConn:
+            if not isConn: 
                 print ('Подключился клиент: ', addr, end = ' Название: ')
                 data = conn.recv(1024)
                 isConn = True
                 print(data.decode("utf-8"))
+            
             replyFromClient = conn.recv(1024)
+            
             if not replyFromClient:
                 break
-            if replyFromClient=='/exit':
-                break
+            
             print (str(data.decode("utf-8")) + ' пишет: ' + str(replyFromClient.decode("utf-8")))
+            
+            if replyFromClient.decode('utf-8')=='/exit':
+                break         
             conn.send(replyFromClient.decode("utf-8").upper().encode())
         conn.close()
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 9091))
-server.listen(5)
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', 9090))
+server.listen(5)
 
 while True:
     conn, addr = server.accept()
