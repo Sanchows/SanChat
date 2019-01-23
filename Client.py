ee6c64573ebb64f758db101ace282ff14bce8ba8
sanchows <sanyasach@mail.ru>
2019-01-24 00:22:35 +0300

    view connected clients to the server; code improvements (handlers added and more)

--- Client.py
+++ Client.py
@@ -1,10 +1,10 @@
-import socket
-import threading
-import os
+import socket, threading, os
+
+# 23.01 22:22
+''' 
+Added handler invalid input symbols
 '''
-Added handler event, when is impossible connecting to server
-Added handler for sudden closing of program (50%)
-'''
+
 class otherClients(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -13,7 +13,7 @@ class otherClients(threading.Thread):
         while True:
             try:
                 replyFromOtherClients = sock.recv(1024) 
-                print ('\n'+str(replyFromOtherClients.decode('utf-8')))
+                print (str(replyFromOtherClients.decode('utf-8')))
             except:
                 sock.close()
                 break
@@ -26,8 +26,11 @@ class Client(threading.Thread):
     def run(self):
         while True:
             try:
-                textMsg = str(input(clientName+': '))
-                sock.send(textMsg.encode())
+                #textMsg = str(input('clientName+': '))
+                textMsg = str(input())
+                if textMsg.isspace():
+                    continue
+                sock.send(textMsg.strip().encode())
                 if textMsg == '/exit':
                     break
             except:
@@ -36,14 +39,21 @@ class Client(threading.Thread):
         os._exit(1)
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 9092))
+    sock.connect(('localhost', 9088))
 except:
     print('Error connecting to server')
     os._exit(1)
 print ('You are connected to server. Enter your name: ', end='')
-clientName = input()
-
-sock.send(clientName.encode())  
+while True:
+    clientName = str(input())
+    if clientName.isspace(): #if is empty or all symbols of string this is spaces
+        print ("Please, enter your name: ", end='')
+        continue
+    if not clientName.isalnum():
+        print ("Допустимые символы: A-z,0-9.\nPlease, enter your name: ", end='')
+        continue
+    sock.send(clientName.encode())  
+    break
 
 Client().start()
 otherClients().start()
--- Server.py
+++ Server.py
@@ -1,58 +1,84 @@
-import socket
-import threading
-import time
+import socket, threading, time
 
-# 21.01 15:25
 '''
-Improved interactionclient with the server and other clients when connecting / disconnecting
-Added handler for sudden closing of program (50%)
+# 24.01 00:05
+the ability to view connected clients to the server
 '''
 
+'''
+need to do:
+ method editingClients()
+ time
+
+
+ cryption
+ protocol
+ DB
+'''
 class ClientThread(threading.Thread):
     def __init__(self, conn, addr):
-        self.conn = conn
-        self.addr = addr
+        self.conn = conn #socket
+        self.addr = addr #list: [0] - ip , [1] - id
         threading.Thread.__init__(self)
-
+            
     def run(self):
-        isConn = False
+        isConn = False # for 
         while True:
-            if not isConn: # if the client connects for the first time
-                data = self.conn.recv(1024)
-                print ('Connected: ' + 'IP ['+ str(self.addr[0]) + '] ID[' + str(self.addr[1]) + '] N['+ data.decode('utf-8') + ']')
-                isConn = True
+            # if the client connects for the first time:
+            if not isConn: 
+                inputNameClient = self.conn.recv(1024)
+                IP_CLIENT = str(self.addr[0])
+                ID_CLIENT = str(self.addr[1])
+                NAME_CLIENT = inputNameClient.decode("utf-8")
+
+                print("Connected: IP[{0}] ID[{1}] N[{2}]".format(IP_CLIENT, ID_CLIENT, NAME_CLIENT))
                 clients.append(self.conn)
+                names_clients.append(NAME_CLIENT)
+                self.conn.send("=================================\nWelcome to SanChat, bro ;3".encode())
                 for client in clients:
                     if client != self.conn:
-                        txtMsg = '\n<<<| ' + data.decode('utf-8') + ' joined the chat! |>>>'
+                        txtMsg = "<--- {0} joined the chat! --->".format(NAME_CLIENT)
                         client.send(txtMsg.encode())
-            
+                isConn = True
+        #==== ПРИЕМ СООБЩЕНИЯ ===============================================
             replyFromClient = self.conn.recv(1024)
-
+            INC_MESSAGE = replyFromClient.decode("utf-8")
+            
+            if INC_MESSAGE == '/online':
+                FOR_ONLINE = '------ NOW CHATTING ------\n'
+                for name in names_clients:
+                    FOR_ONLINE += name + '\n'
+                FOR_ONLINE += '---------------------------'
+                self.conn.send(str(FOR_ONLINE).encode())
+                continue
             for client in clients:
                 if client != self.conn:
-                    txtMsg = data.decode('utf-8') + ': ' + replyFromClient.decode('utf-8') 
+                    txtMsg = "{0}: {1}".format(NAME_CLIENT, INC_MESSAGE)
                     client.send(txtMsg.encode())
             
             if not replyFromClient:
                 break
-            print (str(data.decode("utf-8")) + ' wrote: ' + str(replyFromClient.decode("utf-8")))
+            print ('{0} wrote: {1}'.format(NAME_CLIENT, str(INC_MESSAGE)))
                 
-            if replyFromClient.decode('utf-8')=='/exit':
+            if INC_MESSAGE=='/exit':
                 break
-        print ('Disconnected: ' + 'IP ['+ str(self.addr[0]) + '] ID[' + str(self.addr[1]) + '] N['+ data.decode('utf-8') + ']')
+        #====================================================================
+        print("Disconnected: IP[{0}] ID[{1}] N[{2}]".format(IP_CLIENT, ID_CLIENT, NAME_CLIENT))
         for client in clients:
             if client != self.conn:
-                txtMsg = '\n<<<| ' + data.decode('utf-8') + ' has exit the chat |>>>'
+                txtMsg = "\n<--- {0} has exit the chat --->".format(NAME_CLIENT)
                 client.send(txtMsg.encode())
-        clients.remove(self.conn)         
-        self.conn.close()
 
+        clients.remove(self.conn)
+        names_clients.remove(NAME_CLIENT)         
+        self.conn.close()
+#==============================================================================================================
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.bind(('localhost', 9092))
+conn.bind(('localhost', 9088))
 conn.listen(5)
 
 clients = []
+names_clients = []
 
 while True: 
     connS, addr = conn.accept()
